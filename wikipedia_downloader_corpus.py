ec3129397634f4cc4f06f8e97c0485cac1d6890d
Sergio Rodríguez Calvo <sergiorodriguezcalvo@gmail.com>
2017-04-21 15:13:04 +0200

    Fix wikipedia script

--- wikipedia_downloader_corpus.py
+++ wikipedia_downloader_corpus.py
@@ -1,10 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import wikipedia
 from tqdm import tqdm
 
 def save(path, filename, content, iso):
-	file = open('./wikipedia/%s/%s.%s'%(path,filename,iso), "w+")
-	file.write(content)
-	file.close
+    file = open('./wikipedia/%s/%s.%s'%(path,filename,iso), "w+")
+    file.write(content.encode('utf-8').strip())
+    file.close
 
 languages_directories = list(open('languages.txt','r'))[0].rstrip().split(',') #Remove \n
 
@@ -14,11 +17,19 @@ topics = list(open('topics.txt','r'))[0].rstrip().split(',') #Remove \n
 
 languages_dict = dict()
 for i in range(0,len(languages)):
-	languages_dict[languages[i]] =languages_directories[i] 
+    languages_dict[languages[i]] =languages_directories[i] 
 
 
 for lang_iso, lang in tqdm(languages_dict.items()):
-	for topic in topics:
-		topic_page = wikipedia.page(topic)
-		topic_content = topic_page.content
-		save(lang, topic, topic_content, lang_iso)
+    for topic in topics:
+        print("Languaje: " + lang_iso + ", Topic: " + topic)
+        try:
+            wikipedia.set_lang(lang_iso)
+            topic_page = wikipedia.page(topic)
+            if topic_page:
+                topic_content = topic_page.content
+                if topic_content:
+                    save(lang, topic, topic_content, lang_iso)
+        except wikipedia.exceptions.PageError:
+            #if a "PageError" was raised, ignore it and continue to next link
+            continue
